6c970b8049c63e99513cb149ac62eaeca26f4d83
David Rueck <drueck@gmail.com>
2025-12-17 20:22:52 -0800

    Day 7, Part 2

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -7,48 +7,107 @@ use std::{env, fs};
 fn main() {
     let input_filename = env::args().nth(1).expect("please supply an input filename");
     let input = fs::read_to_string(input_filename).expect("failed to read input");
-    let mut grid = Grid::new(&input);
+
+    let mut grid = Grid::of_u8_with_count(&input);
+    let mut splits = 0;
 
     for col in 0isize..grid.cols as isize {
-        if grid.get(0, col).unwrap() == b'S' {
-            grid.set(1, col, b'|');
+        if grid.get(0, col).unwrap().val == b'S' {
+            grid.set(1, col, ValWithCount::new(b'|', 1));
+            break;
         }
     }
 
-    let mut splits = 0;
-
     for row in 1isize..grid.rows as isize - 2 {
         for col in 0isize..grid.cols as isize {
-            if grid.get(row, col).unwrap() == b'|' {
-                if grid.get(row + 1, col).unwrap() == b'^' {
-                    grid.set(row + 2, col - 1, b'|');
-                    grid.set(row + 2, col + 1, b'|');
-                    splits += 1;
-                } else {
-                    grid.set(row + 2, col, b'|');
+            let cell = grid.get(row, col).unwrap();
+            if cell.val == b'|' {
+                match grid.get(row + 1, col).unwrap() {
+                    ValWithCount {
+                        val: b'^',
+                        count: _,
+                    } => {
+                        grid.set(
+                            row + 2,
+                            col - 1,
+                            ValWithCount::new(
+                                b'|',
+                                grid.get(row + 2, col - 1).unwrap().count + cell.count,
+                            ),
+                        );
+                        grid.set(
+                            row + 2,
+                            col + 1,
+                            ValWithCount::new(
+                                b'|',
+                                grid.get(row + 2, col + 1).unwrap().count + cell.count,
+                            ),
+                        );
+                        splits += 1
+                    }
+                    _ => {
+                        grid.set(
+                            row + 2,
+                            col,
+                            ValWithCount::new(
+                                b'|',
+                                grid.get(row + 2, col).unwrap().count + cell.count,
+                            ),
+                        );
+                    }
                 }
             }
         }
     }
 
+    let timelines: usize = (0..grid.cols as isize)
+        .map(|col| grid.get(grid.rows as isize - 1, col).unwrap().count)
+        .sum();
+
     println!("The number of splits was {splits}");
+    println!("The number of timelines were {timelines}");
 }
 
-struct Grid {
-    data: Vec<u8>,
+struct Grid<T>
+where
+    T: Copy,
+{
+    data: Vec<T>,
     rows: usize,
     cols: usize,
 }
 
-impl Grid {
-    fn new(input: &str) -> Self {
+#[derive(Clone, Copy, PartialEq, Eq)]
+struct ValWithCount {
+    val: u8,
+    count: usize,
+}
+
+impl ValWithCount {
+    fn new(val: u8, count: usize) -> Self {
+        Self { val, count }
+    }
+}
+
+impl Grid<ValWithCount> {
+    fn of_u8_with_count(input: &str) -> Self {
         let cols = input.as_bytes().iter().position(|c| *c == b'\n').unwrap();
-        let data = input.replace("\n", "").into_bytes();
+        let data: Vec<ValWithCount> = input
+            .replace("\n", "")
+            .as_bytes()
+            .iter()
+            .map(|&b| ValWithCount::new(b, 0))
+            .collect();
         let rows = data.len() / cols;
         Self { data, rows, cols }
     }
+}
 
-    fn get(&self, row: isize, col: isize) -> Option<u8> {
+impl<T> Grid<T>
+where
+    T: Copy,
+{
+    fn get(&self, row: isize, col: isize) -> Option<T> {
         if let Some(index) = self.coord_to_index(row, col) {
             Some(self.data[index])
         } else {
@@ -56,7 +115,7 @@ impl Grid {
         }
     }
 
-    fn set(&mut self, row: isize, col: isize, val: u8) {
+    fn set(&mut self, row: isize, col: isize, val: T) {
         if let Some(index) = self.coord_to_index(row, col) {
             self.data[index] = val;
         }
